7e6ab27ef4b04843a48180bb106e7b63518ba219
Nykia Le'Shae <designs@nykialeshae.com>
2020-05-02 19:51:07 -0500

    status

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function displayTime() {
-    let date = new Date();
-    let time = date.toLocaleTimeString();
-    document.querySelector('.clock').textContent = time;
-  }
 
-  displayTime();
-  const createClock = setInterval(displayTime, 1000);
+// A "Stop" button to pause/stop it.
+// A "Reset" button to reset the time back to 0.
+// The time display to show the number of seconds elapsed, rather than the actual time.
+
+
+
+const secondsElement = document.getElementById('seconde-elapsed')
+let seconds = 0
+
+// A "Start" button to start the stopwatch running.
+document.getElementById('start').onclick = function() {
+    setInterval(function() {
+        seconds++
+        secondsElement.innerText = seconds
+    }, 1000);
+}

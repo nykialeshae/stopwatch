afeb5921e07e83472a5112de5dcc830e23b9c24b
Nykia Le'Shae <designs@nykialeshae.com>
2020-05-02 20:16:15 -0500

    Reset button works

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-// A "Stop" button to pause/stop it.
-// A "Reset" button to reset the time back to 0.
 // The time display to show the number of seconds elapsed, rather than the actual time.
 
 
@@ -27,4 +25,10 @@ startButton.onclick = function() {
 document.getElementById('stop').onclick = function() {
     clearInterval(interval)
     startButton.removeAttribute('disabled', false)
+}
+
+// A "Reset" button to reset the time back to 0.
+document.getElementById('reset').onclick = function() {
+    seconds = 0
+    displaySeconds()
 }
